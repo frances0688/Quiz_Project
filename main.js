cd94e41240a71a0eaf484128bf1467aa6eb22b9b
Frances Morales Velilla <frances0688@gmail.com>
2023-10-18 00:45:53 +0200

    Add progress bar functionality, add score, some styling

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ const resultsNav = document.getElementById("resultsNav");
 const results = document.getElementById("results");
 const name = document.getElementById("name");
 const homeBtn = document.getElementById("homeBtn");
-const progressBar = document.getElementById("progressBar");
+const progressBar = document.querySelector(".progress-bar");
+const progressBarDiv =
+	document.getElementById("progressBar");
 const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById(
@@ -19,7 +21,8 @@ const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById(
 	"answer-buttons"
 );
-
+const scoreText = document.getElementById("scoreText");
+const restartBtn = document.getElementById("restart-btn");
 const user = {
 	name: "",
 	results: [
@@ -64,7 +67,7 @@ function selectCategory(e) {
 function showCategories(categoriesData) {
 	categoriesData.forEach((category) => {
 		const button = document.createElement("button");
-		button.innerText = category.name;
+		button.innerHTML = category.name;
 		button.setAttribute("value", category.id);
 		categories.appendChild(button);
 		button.addEventListener("click", selectCategory);
@@ -85,7 +88,6 @@ function getQuestions(chosenCategory) {
 		)
 		.then((res) => {
 			questionsArray = res.data.results;
-			console.log(questionsArray);
 			questionsArray.forEach((question) => {
 				const incorrectAnswers = question.incorrect_answers;
 				question = {
@@ -110,7 +112,11 @@ function getQuestions(chosenCategory) {
 		.catch((err) => console.error(err));
 }
 
-// Hide Views
+/*
+	SPA
+*/
+
+//Hide Views
 function hideView() {
 	home.classList.add("d-none");
 	categories.classList.add("d-none");
@@ -138,26 +144,32 @@ function goPlay() {
 function goResults() {
 	hideView();
 	results.classList.remove("d-none");
+	scoreText.innerHTML = `${score}/10`;
 }
 
 // Start Game
 let currentQuestionIndex;
+let currentSession;
+let score = 0;
+let progress = -10;
 
 function startGame() {
 	startButton.classList.add("d-none");
 	currentQuestionIndex = 0;
+	progressBar.setAttribute("style", `width: ${progress}%`);
 	questionContainerElement.classList.remove("d-none");
 	answerButtonsElement.classList.remove("d-none");
-	progressBar.classList.remove("d-none");
+	progressBarDiv.classList.remove("d-none");
 	setNextQuestion();
 }
 
 // Show Question
 function showQuestion(question) {
-	questionElement.innerText = question.question;
+	questionElement.innerHTML = question.question;
 	question.answers.forEach((answer) => {
 		const button = document.createElement("button");
-		button.innerText = answer.text;
+		button.innerHTML = answer.text;
+		button.classList.add("btn", "btn-secondary");
 		if (answer.correct) {
 			button.dataset.correct = true;
 		}
@@ -170,19 +182,25 @@ function showQuestion(question) {
 function setNextQuestion() {
 	resetState();
 	showQuestion(questions[currentQuestionIndex]);
+	progressBar.setAttribute(
+		"style",
+		`width: ${(progress += 10)}%`
+	);
 }
 
 // Set status class
 function setStatusClass(element) {
 	if (element.dataset.correct) {
-		element.classList.add("correct");
+		element.classList.add("btn-success");
 	} else {
-		element.classList.add("wrong");
+		element.classList.add("btn-light");
 	}
 }
 
 // Select answer
-function selectAnswer() {
+function selectAnswer(e) {
+	e.target.dataset.correct ? (score += 1) : null;
+	console.log(score);
 	Array.from(answerButtonsElement.children).forEach(
 		(button) => {
 			setStatusClass(button);
@@ -191,7 +209,7 @@ function selectAnswer() {
 	if (questions.length > currentQuestionIndex + 1) {
 		nextButton.classList.remove("d-none");
 	} else {
-		goResults();
+		setTimeout(goResults, 3000);
 	}
 }
 
@@ -199,6 +217,16 @@ function selectAnswer() {
 function resetState() {
 	nextButton.classList.add("d-none");
 	answerButtonsElement.innerHTML = "";
+}
+
+//Restart Game
+function restart() {
+	score = 0;
+	progress = -10;
+	categories.innerHTML = "";
+	resetState();
+	results.classList.add("d-none");
+	goCategories();
 }
 
 // Event Listeners
@@ -216,3 +244,5 @@ nextButton.addEventListener("click", () => {
 	currentQuestionIndex++;
 	setNextQuestion();
 });
+
+restartBtn.addEventListener("click", restart);
